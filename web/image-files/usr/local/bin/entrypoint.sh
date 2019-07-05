8631519fd2d35caa8b0129f3f93c176c99961c1d
Aditya Satrya <aditya.satrya@gmail.com>
2019-07-05 14:56:32 +0700

    initial commit

--- web/image-files/usr/local/bin/entrypoint.sh
+++ web/image-files/usr/local/bin/entrypoint.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
 
-printf "Checking database connection...\n\n"
-mysql_ready() {
-    mysqladmin ping --host=$MYSQL_HOST --user=root --port=$MYSQL_PORT --password=$MYSQL_ROOT_PASSWORD > /dev/null 2>&1
-}
-
-while !(mysql_ready)
-do
-    sleep 3
-    echo "Waiting for database connection ..."
-done
-
-printf "Starting apache...\n\n"
-apachectl -DFOREGROUND
+# tail -f /dev/null
+node app.js
